d7b0a8ffacea5fd16f9be5b9a6f292f19c4c16a4
Stijn Bosch <stijndebosch@gmail.com>
2025-04-17 20:18:44 +0200

    test_linalggg

--- test_linalg.cpp
+++ test_linalg.cpp
@@ -2,12 +2,30 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 int main() {
-    std::vector<std::vector<double>> m1_data = {
+    vector<vector<double>> m1_data = {
         {1.0, 3.0, 4.0},
         {6.0, 7.0, 3.0}
     }; 
     Matrix m1 = Matrix(2, 3, m1_data);
-    std::cout << m1;
+
+    Matrix transposed_m1 = m1.transposed();
+
+    vector<vector<double>> m2_data {
+        {2},
+        {4},
+        {1}
+    };
+    Matrix m2 = Matrix(3, 1, m2_data);
+    cout << m1;
+    cout << transposed_m1 << endl;
+    cout << dot_product({4, 5, 6}, {1, 2, 3}) << endl;
+
+    Matrix m3 = m1 * m2;
+    cout << "  " << m3 << "    " << endl;
+
+    cout << m1[1][1];
     return 0;
 }
