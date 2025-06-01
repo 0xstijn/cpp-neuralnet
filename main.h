352bd8dda1766cd612fdb3c54e52066aaf64c384
JesperSnake <jesperklicks@gmail.com>
2025-06-01 15:42:09 +0200

    Added a second working modular Model class with a working feed forward and loss calculation

--- main.h
+++ main.h
@@ -4,20 +4,33 @@
 class Layer{
     public:
         // Constructor
-        Layer(int neuron_amount, int prev_neuron_amount);
-
+        Layer(int neuron_amount, int prev_neuron_amount, std::string activation = "reLu");
+        // Activation function
+        std::string activation;
         Matrix weights;
         std::vector<double> biases;
         //Forward pass
         std::vector<double> forward(std::vector<double> input);
+
        
 
 };
-
-
 class Model{
     public:
+        // Constructor
+        Model(std::vector<int> layer_sizes, std::vector<std::string> activations);
+        //Full layer vector
+        std::vector<Layer> layers;
+        // Activation function
+        std::string activation_function;
+        int model_size;
+        // Forward pass       
+        std::vector<double> feed_forward(std::vector<double> input);
+        //Loss
+        std::vector<double> loss(std::vector<double> output, std::vector<double> target);
+        
 
-        Model(int* array, int size);
 
 };
+std::vector<double> add_vectors(std::vector<double> v1, std::vector<double> v2);
+
